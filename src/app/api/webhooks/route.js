d310644ce15783fb3a715f892a94cf55034d929f
Nextjs <tech.united.it@gmail.com>
2025-02-27 10:48:10 +0200

    clerk sign in key

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -7,6 +7,7 @@ import { headers } from 'next/headers'
 //import { WebhookEvent } from '@clerk/nextjs/server'
 
 export async function POST(req) {
+    
   const SIGNING_SECRET = process.env.SIGNING_SECRET
 
   if (!SIGNING_SECRET) {
